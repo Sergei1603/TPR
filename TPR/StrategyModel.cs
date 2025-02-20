8e335ce4191916cb538f2006ffc16edb6092a4e9
Сергей Кузнецов <sergkuz03.ru@yandex.ru>
2025-02-20 13:03:49 +0500

    Создана модель

---
+++ TPR/StrategyModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPR
+{
+    internal class Strategy
+    {
+        public List<List<double>> Profit { get; set; }
+        public List<List<double>>  Probabilites { get; set; }
+    }
+}
