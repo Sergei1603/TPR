5e50167f5e4d5b05be2b81eff30c73972d64b934
Сергей Кузнецов <sergkuz03.ru@yandex.ru>
2025-02-20 12:37:01 +0500

    Добавьте файлы проекта.

---
+++ TPR/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TPR
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
